02278eeec9208e661a79afb40e9c933b72bb4f5a
murin.89@list.ru <murin.89@list.ru>
2023-08-08 16:29:58 +0400

    add new style

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+// Полифилл для метода forEach для NodeList
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+    const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+    const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
+
+    const dropDownListItems = dropDownWrapper.querySelectorAll('.dropdown__list-item');
+    const dropDownInput = dropDownWrapper.querySelector('.dropdown__input__hidden');
+
+    // document.querySelector('.dropdown__list').classList.add('none');
+    dropDownBtn.addEventListener("click", function () {
+        dropDownList.classList.toggle('active');
+        this.classList.add("dropdown__button--active")
+    })
+
+    dropDownListItems.forEach(function (listItem) {
+        listItem.addEventListener("click", function (e) {
+            e.stopPropagation();
+            dropDownBtn.innerText = this.innerText;
+            dropDownBtn.focus();
+            dropDownInput.value = this.dataset.value;
+            dropDownList.classList.remove("active");
+        })
+    });
+
+    // клик снаружи
+    document.addEventListener('click', function (e) {
+        if (e.target !== dropDownBtn) {
+            dropDownBtn.classList.remove("dropdown__button--active")
+            dropDownList.classList.remove('active');
+        }
+    });
+
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Tab' || e.key === 'Escape') {
+            dropDownBtn.classList.remove("dropdown__button--active")
+            document.querySelector('.dropdown__list').classList.remove('active');
+        }
+    })
+});
 const colDescs = document.querySelectorAll(".questions__col__desc");
 const colBtns = document.querySelectorAll(".questions__col__btn");
 
@@ -15,5 +63,21 @@ colBtns.forEach(function (item, index) {
             element.classList.add("none");
         })
         colDescs[index].classList.toggle('none');
+    
+        item.classList.toggle('rorateBtn');
     })
 })
+
+
+$(function() {
+	$('.video__popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
+
